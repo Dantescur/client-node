565e1772dae3f64f32521fcaa800199ddd39d467
Dantescur <daniel@thedaniweb.eu.org>
2025-08-08 20:22:37 -0400

    feat: coins endpoint and types

---
+++ src/api/coins.ts
@@ -0,0 +1,40 @@
+import type { QvaPayClient } from '../helpers/axios'
+import type {
+  CoinsResponse,
+  CoinsV2Params,
+  CoinsV2Response,
+  CoinV2Response,
+} from '../interfaces'
+
+export class Coins {
+  constructor(private readonly client: QvaPayClient) {}
+
+  async getAll(): Promise<CoinsResponse> {
+    return await this.client.request<CoinsResponse>({
+      method: 'GET',
+      url: '/coins',
+    })
+  }
+
+  async getV2(params?: CoinsV2Params): Promise<CoinsV2Response> {
+    const queryParams = new URLSearchParams()
+
+    Object.entries(params || {}).forEach(([key, value]) => {
+      if (value !== undefined) {
+        queryParams.append(key, String(value))
+      }
+    })
+
+    return await this.client.request<CoinsV2Response>({
+      method: 'GET',
+      url: `/coins/v2${queryParams.toString() ? `?${queryParams}` : ''}`,
+    })
+  }
+
+  async getById(id: number): Promise<CoinV2Response> {
+    return await this.client.request<CoinV2Response>({
+      method: 'GET',
+      url: `/coins/${id}`,
+    })
+  }
+}
--- src/interfaces/coin.ts
+++ src/interfaces/coin.ts
@@ -1,18 +1,76 @@
+export interface CoinsResponse {
+  id: number
+  name: string
+  logo: string
+  coins: Coin[]
+}
+
 export interface Coin {
   id: number
-  coins_categories_id: number
+  coinsCategoriesID: number
   name: string
   logo: string
   tick: string
-  fee_in: string
-  fee_out: string
-  min_in: string
-  min_out: string
-  working_data: string
-  enabled_in: number
-  enabled_out: number
-  enabled_p2p: number
+  feeIn: string
+  feeOut: string
+  minIn: string
+  minOut: string
+  workingData: string
+  enabledIn: number
+  enabledOut: number
+  enabledP2P: number
   price: string
-  created_at: null
-  updated_at: string
+  createdAt: null
+  updatedAt: Date | null
+}
+
+export interface CoinsV2Params {
+  enabledIn?: boolean
+  enabledOut?: boolean
+  enabledP2P?: boolean
+}
+
+export interface CoinsV2 {
+  id: string
+  name: string
+  tick: string
+  minIn: string
+  feeIn: string
+  minOut: string
+  feeOut: string
+  enabledIn: boolean
+  enabledOut: boolean
+  enabledP2P: boolean
+  coinsCategoriesID: string
+  price: string
+  logo: string
+  network: null | string
+}
+
+export type CoinsV2Response = CoinsV2[]
+
+export interface CoinV2Response {
+  id: number
+  coinsCategoriesID: number
+  name: string
+  logo: string
+  tick: string
+  feeIn: string
+  feeOut: string
+  minIn: string
+  minOut: string
+  maxIn: number
+  maxOut: number
+  workingData: string
+  enabledIn: number
+  enabledOut: number
+  enabledP2P: number
+  price: string
+  coinCategory: CoinCategory
+}
+
+export interface CoinCategory {
+  id: number
+  name: string
+  logo: string
 }
